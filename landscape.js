87dc1f309c4eba69a89ca76b122b8cdfee5e30ff
vincentiga <61868081+vincentiga@users.noreply.github.com>
2020-03-20 03:23:24 +0100

    Add files via upload

---
+++ landscape.js
@@ -0,0 +1,56 @@
+function black(){
+	let x = 0;
+	let y = 0;
+	let z = 0;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function blue(){
+	let x = 27;
+	let y = 0;
+	let z = 88;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function red(){
+	let x = 100;
+	let y = 0;
+	let z = 0;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function purple(){
+	let x = 73;
+	let y = 0;
+	let z = 86;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function gray(){
+	let x = 165;
+	let y = 165;
+	let z = 165;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function yellow(){
+	let x = 229;
+	let y = 201;
+	let z = 0;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
+function white(){
+	let x = 255;
+	let y = 255;
+	let z = 255;
+	let bgColor = "rgb(" + x + "," + y + "," + z +")";
+	console.log(bgColor);
+	document.body.style.background= bgColor;
+}
